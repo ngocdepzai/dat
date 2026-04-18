7e12d90267db70050dfc72f6a77062e4f0f480fd
huyhoangtech3@gmail.com <nguyenhuy1!>
2026-04-18 10:16:08 +0700

    Fix bugs jump quá xa

--- app/src/main/java/com/hc/dat/model/SessionVerificationInfo.kt
+++ app/src/main/java/com/hc/dat/model/SessionVerificationInfo.kt
@@ -1,7 +1,6 @@
 package com.hc.dat.model
 
 import android.location.Location
-import com.hc.dat.utils.KalmanSpeedFilter
 import com.hc.dat.utils.Utils
 import com.hc.dat.utils.Utils.isValidGpsSpeed
 import com.hc.dat.viewmodel.VerifyResult
@@ -31,44 +30,25 @@ data class SessionVerificationInfo(
         // filter location has lat, long incorrect in vietnam geo
         if (location.latitude > 8 && location.latitude < 23 && location.longitude > 102 && location.longitude < 110
         ) {
-            // calculate distance base on max speed assumption is not more than 100km/h -> not than 28m/s
+            // Loại bỏ các điểm có độ chính xác quá kém (nhảy GPS) ---
+            if (location.hasAccuracy() && location.accuracy > 200f) {
+                Logger.e("GPS ignored - low accuracy: ${location.accuracy}")
+                return
+            }
+
             val calculateDistance: Float = latestLocation?.distanceTo(location) ?: 0.0f
-//            val durationCalculate: Int = if (latestLocation != null) ((location.time - latestLocation!!.time).toInt() / 1000) else 0
             val durationCalculate: Float = if (latestLocation != null) ((location.time - latestLocation!!.time) / 1000f) else 0f
             val speedCalculate = if (durationCalculate > 0f) calculateDistance / durationCalculate else 0f
             Logger.i("HoangSpeed so sánh speed & speedCalculate: " + "calculateDistance: $calculateDistance |  durationCalculate: $durationCalculate | speedCalculate: $speedCalculate | speed: ${location.speed} | latitude: ${location.latitude} | longitude: ${location.longitude}  | time: ${location.time}")
             LogRecorder.i("Lấy GPS thành công so sánh speed & speedCalculate", "calculateDistance: $calculateDistance |  durationCalculate: $durationCalculate | speedCalculate: $speedCalculate | speed: ${location.speed} | latitude: ${location.latitude} | longitude: ${location.longitude}  | time: ${location.time}")
 
-            val isDistanceJump = calculateDistance > (28f * durationCalculate)
             val durationTooLong = durationCalculate > 30f
 
-            if (latestLocation == null || (speedCalculate <= 28f && !isDistanceJump)) {
-                LogRecorder.i("GPS accepted | ",
-                         "distance=$calculateDistance m | " +
-                                "duration=$durationCalculate s | " +
-                                "speedCalculate=$speedCalculate m/s | " +
-                                "speed=${location.speed} m/s | " +
-                                "accuracy=${location.accuracy} m | " +
-                                "lat=${location.latitude} | " +
-                                "long=${location.longitude}"
-                )
-            } else {
-                LogRecorder.i("GPS ignored - maybe jump | ",
-                         "distance=$calculateDistance m | " +
-                                "duration=$durationCalculate s | " +
-                                "speedCalculate=$speedCalculate m/s | " +
-                                "maxAllowedDistance=${28f * durationCalculate} m | " +
-                                "isDistanceJump=$isDistanceJump | " +
-                                "speed=${location.speed} m/s | " +
-                                "accuracy=${location.accuracy} m | " +
-                                "oldLat=${latestLocation?.latitude} | " +
-                                "oldLong=${latestLocation?.longitude} | " +
-                                "newLat=${location.latitude} | " +
-                                "newLong=${location.longitude}"
-                )
-            }
+            // Nếu 1 điểm GPS mới cách điểm cũ hơn 1km thì hệ thống sẽ không cập nhật tọa độ đó vào dữ liệu gửi lên server
+            // (tránh vẽ đường kẻ xuyên thành phố).
+            if (latestLocation == null || speedCalculate <= 28f && calculateDistance < 3000f) {
 
-            if (latestLocation == null || speedCalculate <= 28) {
+                // Cập nhật khoảng cách và thời gian cho đoạn di chuyển này
                 latestLocation?.apply {
                     distance = this.distanceTo(location)
                     duration = location.time - this.time
@@ -87,8 +67,11 @@ data class SessionVerificationInfo(
             } else {
                 Logger.e("Warning: Location maybe fake, because distance get than 40m per second -> ignore this location")
                 LogRecorder.e("Warning: Location maybe fake, ","because distance get than 40m per second -> ignore this location")
+                // Gán distance = 0 để server không cộng dồn quãng đường sai
+                this.distance = 0f
+                this.duration = 0L
 
-                // nếu quá lâu mới có GPS tiếp theo thì chấp nhận cập nhật latestLocation
+                // nếu quá lâu mới có GPS tiếp theo thì chấp nhận cập nhật latestLocation làm mốc mới
                 if (durationTooLong) {
                     latestLocation = location
                     Logger.e("Accept new latestLocation because duration too long: $durationCalculate s")
